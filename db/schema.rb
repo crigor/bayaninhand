9c90d1478f21b9dd1475843aa8c0bbf946f8273d
Christopher Rigor <crigor@gmail.com>
2011-11-16 22:22:56 +0800

    remove date from events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111114142756) do
+ActiveRecord::Schema.define(:version => 20111116141756) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -50,7 +50,6 @@ ActiveRecord::Schema.define(:version => 20111114142756) do
     t.integer  "organization_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.date     "date"
     t.text     "address"
     t.string   "map"
     t.date     "start_date"
