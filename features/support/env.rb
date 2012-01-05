f2b31f12cd67babe3515a4e49b7b9aec8277d6c0
Christopher Rigor <crigor@gmail.com>
2012-01-06 01:44:13 +0800

    fix an event scenario, use the home page when logging in on cucumber

--- features/support/env.rb
+++ features/support/env.rb
@@ -10,7 +10,7 @@ require 'cucumber/rails'
 # order to ease the transition to Capybara we set the default here. If you'd
 # prefer to use XPath just remove this line and adjust any selectors in your
 # steps to use the XPath syntax.
-Capybara.default_selector = :css
+#Capybara.default_selector = :css
 
 # By default, any exception happening in your Rails application will bubble up
 # to Cucumber so that your scenario will fail. This is a different from how 
