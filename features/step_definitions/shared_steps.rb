eac1c7bcf1a79ca7877b81bed4e5ca836fa2c1a1
Christopher Rigor <crigor@gmail.com>
2012-01-06 01:52:06 +0800

    change the ids of the login fields at the top of the page to fix some scenarios, the devise login form uses the same ids add the notice on the page to fix some scenarios

--- features/step_definitions/shared_steps.rb
+++ features/step_definitions/shared_steps.rb
@@ -1,8 +1,8 @@
 Given /^"([^"]*)" is logged in$/ do |email|
   steps %Q{
   Given I am on the home page
-  When I fill in "user_email" with "#{email}"
-  And I fill in "user_password" with "password"
+  When I fill in "user_email_top" with "#{email}"
+  And I fill in "user_password_top" with "password"
   And I press "Login"
   }
 
