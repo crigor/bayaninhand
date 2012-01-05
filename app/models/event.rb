edc06978833d46077cf5f0231d15e83a72c47f5d
Christopher Rigor <crigor@gmail.com>
2012-01-06 01:59:30 +0800

    use end date on upcoming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ActiveRecord::Base
   validates_presence_of :title, :description, :organization, :start_date
   has_many :participations
   has_many :volunteers, :source => :user, :through => :participations
-  scope :upcoming, lambda { where("start_date >= ?", Date.today) }
+  scope :upcoming, lambda { where("end_date >= ?", Date.today) }
   has_and_belongs_to_many :categories
   has_and_belongs_to_many :event_types
   has_attached_file :image, :styles => { :medium => "250x200>", :small => "220x140>" }
