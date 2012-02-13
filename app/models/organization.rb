9b79b478d683903d91dd4ae7eee75533b87bd3b0
Christopher Rigor <crigor@gmail.com>
2012-02-13 20:59:56 +0800

    check validity of email address of organizations

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,5 +2,6 @@ class Organization < ActiveRecord::Base
   has_many :events
   validates_presence_of :name, :address, :contact_person, :phone_number, :email, :website, :organization_type, :country, :mission_statement, :description
   validates_numericality_of :phone_number, :only_integer => true, :message => "should be a valid number"
+  validates_format_of :email, :with => /^.+@.+\..+$/, :message => "should be valid" # simple check
   belongs_to :organization_type
 end
