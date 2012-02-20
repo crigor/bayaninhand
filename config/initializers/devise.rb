dd7c3504a6cf74f36b0cf2efc6e2262d32a70f33
gerald <gerald@gerald-laptop.(none)>
2012-02-20 22:36:47 +0800

    Add custom redirect for failed login.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -201,4 +201,7 @@ Devise.setup do |config|
   #   manager.intercept_401 = false
   #   manager.default_strategies(:scope => :user).unshift :some_external_strategy
   # end
+  config.warden do |manager|
+    manager.failure_app = LoginFailure
+  end
 end
