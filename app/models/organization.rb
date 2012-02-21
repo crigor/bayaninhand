745d354257204d2e2aaed4fd1cd8588c99dcf6eb
gerald <gerald@gerald-laptop.(none)>
2012-02-22 00:55:30 +0800

    Add categories to organizations

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -4,4 +4,5 @@ class Organization < ActiveRecord::Base
   validates_numericality_of :phone_number, :only_integer => true, :message => "should be a valid number"
   validates_format_of :email, :with => /^.+@.+\..+$/, :message => "should be valid" # simple check
   belongs_to :organization_type
+  has_and_belongs_to_many :categories
 end
