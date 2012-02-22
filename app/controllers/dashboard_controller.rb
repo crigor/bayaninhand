87c8937c3cdbdf948d7cb8a292ec6a3ade4bc77b
Christopher Rigor <crigor@gmail.com>
2012-02-22 20:04:11 +0800

    add design for dashboard events page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,12 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user!
 
   def volunteer
-   @user = current_user
-   @upcoming_events = @user.upcoming_events
+    @user = current_user
+    @upcoming_events = @user.upcoming_events
+  end
+
+  def events
+    @user = current_user
+    @upcoming_events = @user.upcoming_events
   end
 end
