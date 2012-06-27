7c81ff36c7e4ddbf4cdf9c6de9152117da58c303
Christopher Rigor <crigor@gmail.com>
2012-06-27 21:02:30 +0800

    set up email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,5 @@ Bayaninhand::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.action_mailer.delivery_method = :sendmail
 end
