0e43361e02c0dd61013431b11e34abdf8d816365
Christopher Rigor <crigor@gmail.com>
2013-03-13 09:54:38 -0700

    change activeadmin organization index page

--- app/admin/organizations.rb
+++ app/admin/organizations.rb
@@ -1,2 +1,20 @@
 ActiveAdmin.register Organization do
+  index do
+    column :id
+    column :logo do |organization|
+      image_tag organization.image.url(:small)
+    end
+    column :contact_person
+    column :phone_number
+    column :email
+    column :description do |organization|
+      organization.description[0..100]
+    end
+    column :website
+    column :created_at
+    column :mission_statement do |organization|
+      organization.mission_statement[0..100]
+    end
+    default_actions
+  end
 end
