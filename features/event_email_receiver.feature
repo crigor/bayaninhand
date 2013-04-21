15464798d3be3d1b74e948d315d27a011c0deb0f
Christopher Rigor <crigor@gmail.com>
2013-04-21 14:46:54 +0800

    add tests for event email receiver

---
+++ features/event_email_receiver.feature
@@ -0,0 +1,13 @@
+Feature: EventEmailReceiver
+  As a user
+  I want to be able to reply to an email
+  So that I can communicate with the organizer
+
+  Scenario: User receives an email after joining an event then replies
+    Given I am logged in
+    Given an event exists with a title of "Event"
+    And I am on the event page of "Event"
+    And I click the volunteer button
+    Then "test@ivolunteer.com.ph" should receive an email
+    When I reply to the email notification for "Event"
+    Then an event message is created for "Event"
---
+++ features/step_definitions/event_email_receiver_steps.rb
@@ -0,0 +1,27 @@
+When /^I reply to the email notification for "([^"]*)"$/ do |event_title|
+  event = Event.where(:title => event_title).first
+  email = "event-#{event.id}@mail-test.ivolunteer.com.ph"
+  params =  {
+      "SPF" => "pass",
+      "charsets" => '{"from": "UTF-8", "subject": "UTF-8", "text": "ISO-8859-1", "to": "UTF-8"}',
+      "dkim" => "none",
+      "envelope" => JSON.generate({
+        "to" => [email],
+        "from" => "test@ivolunteer.com.ph"
+      }),
+      "from" => "Test <test@ivolunteer.com.ph>",
+      "headers" => "",
+      "subject" => "Thank you",
+      "text" => "I have a question regarding this event.",
+      "html" => "<strong>I have a question regarding this event.</strong>",
+      "to" => email
+  }
+  post emails_path(params)
+end
+
+Then /^an event message is created for "([^"]*)"$/ do |event_title|
+ event = Event.where(:title => event_title).first
+ user = User.where(:email => "test@ivolunteer.com.ph").first
+ participation = Participation.where(:event_id => event.id, :user_id => user.id).first
+ EventMessage.where(:participation_id => participation.id, :user_id => user.id).first.should_not be_nil
+end
