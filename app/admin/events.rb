f8c78931913b783a6ecb63123fb7535b62d2c1e8
Christopher Rigor <crigor@gmail.com>
2013-09-09 21:03:35 +0800

    Add organization details on admin events page.

--- app/admin/events.rb
+++ app/admin/events.rb
@@ -1,3 +1,21 @@
 ActiveAdmin.register Event do
-  
+  index do
+    column :id
+    column :title
+    column :description
+    column :address
+    column :start_date
+    column :end_date
+    column :start_time
+    column :end_time
+    column :volunteers_needed
+    column "Volunteers" do |event|
+      event.volunteers.count
+    end
+    column :organization
+    column "Org Id" do |event|
+      event.organization.id
+    end
+    default_actions
+  end
 end
